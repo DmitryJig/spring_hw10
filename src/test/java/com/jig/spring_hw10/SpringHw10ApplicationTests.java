8e0e01eedb7a9de2dd7c30e21f89ffa15a383c4f
dima <jd2344430@gmail.com>
2022-12-03 14:13:26 +0500

    master_commit

---
+++ src/test/java/com/jig/spring_hw10/SpringHw10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jig.spring_hw10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHw10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
